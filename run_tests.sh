05c026a330f0ffd5b95176658985f253b62f1697
Konstantinos Papadopoulos <konpap@intracom-telecom.com>
2015-10-14 16:34:44 +0300

    Fix of paths at run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -9,8 +9,8 @@ export RESULTS_DIR=$WORKSPACE/$JOB_NAME
 
 python3.4 $WORKSPACE/stress_test/nstat_orchestrator.py \
           --test=$JOB_TYPE \
-          --ctrl-base-dir=$WORKSPACE \
-          --sb-generator-base-dir=$WORKSPACE \
+         --ctrl-base-dir=$WORKSPACE/ \
+         --sb-generator-base-dir=$WORKSPACE/ \
           --json-config=$WORKSPACE/stress_test/sample_test_confs/$JOB_NAME".json" \
           --json-output=$WORKSPACE/$JOB_NAME"results.json" \
           --html-report=report.html \
