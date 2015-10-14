e4a64c6ab4298da12168b23f724efe5977a49743
panageo <panageo@intracom-telecom.com>
2015-10-14 13:20:43 +0300

    Update travis job files

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
-echo "Hello from :"$(pwd)
+# build tools
+#-------------------------------------------------------------------------------
+apt-get install -y \
+snmp \
+snmpd \
+libpcap-dev \
+autoconf \
+make \
+automake \
+libtool \
+libconfig-dev
 
-df -h
-free
-cat /proc/cpuinfo
+# ssh service & java installation
+#-------------------------------------------------------------------------------
+apt-get install -y \
+openssh-client \
+openssh-server \
+openjdk-7-jdk
+
+JOB_NAME="sb_active_scalability_mtcbench"
+export RESULTS_DIR=$WORKSPACE/$JOB_NAME
+
+python3.4 $WORKSPACE/stress_test/nstat_orchestrator.py \
+          --test=$JOB_NAME \
+          --ctrl-base-dir=$WORKSPACE \
+          --sb-generator-base-dir=$WORKSPACE \
+          --json-config=$WORKSPACE/stress_test/sample_test_confs/$JOB_NAME".json" \
+          --json-output=$WORKSPACE/$JOB_NAME"results.json" \
+          --html-report=report.html \
+          --output-dir=$WORKSPACE/results/
