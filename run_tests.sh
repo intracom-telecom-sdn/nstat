384f5c5acf95d02a590efbb30131547fd9b0ea5c
Konstantinos Papadopoulos <konpap@intracom-telecom.com>
2015-10-14 15:51:20 +0300

    Correct json file name

--- run_tests.sh
+++ run_tests.sh
@@ -2,11 +2,13 @@
 
 
 
-JOB_NAME="sb_active_scalability_mtcbench"
+JOB_NAME="lithium_RPC_sb_active_scalability_mtcbench"
+JOB_TYPE="sb_active_scalability_mtcbench"
+
 export RESULTS_DIR=$WORKSPACE/$JOB_NAME
 
 python3.4 $WORKSPACE/stress_test/nstat_orchestrator.py \
-          --test=$JOB_NAME \
+          --test=$JOB_TYPE \
           --ctrl-base-dir=$WORKSPACE \
           --sb-generator-base-dir=$WORKSPACE \
           --json-config=$WORKSPACE/stress_test/sample_test_confs/$JOB_NAME".json" \
