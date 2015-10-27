0026beea95ec26b8763feae270e79872f86de8a5
Konstantinos Papadopoulos <konpap@intracom-telecom.com>
2015-10-27 09:57:44 +0200

    Add run_sample_tests for executing sample tests in Travis

---
+++ stress_test/sample_test_confs/run_sample_tests.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3.4
+
+# Copyright (c) 2015 Intracom S.A. Telecom Solutions. All rights reserved.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License v1.0 which accompanies this distribution,
+# and is available at http://www.eclipse.org/legal/epl-v10.html
+
+"""
+Runner for sample tests
+"""
+
+import os
+
+
+def run_tests():
+    """
+    Method for running sample tests
+    """
+
+    pass
+
+if __name__ == '__main__':
+    run_tests()
