0f4666430201dd5b6f57d70367a8f72f04252dfc
Konstantinos Papadopoulos <konpap@intracom-telecom.com>
2015-11-11 22:22:05 +0200

    Remove generator relevant stuff from cbench_utils

--- stress_test/cbench_utils.py
+++ stress_test/cbench_utils.py
@@ -4,7 +4,7 @@
 # terms of the Eclipse Public License v1.0 which accompanies this distribution,
 # and is available at http://www.eclipse.org/legal/epl-v10.html
 
-""" Reusable functions for processes that are generator related """
+""" Reusable functions for processes that are cbench related """
 
 import logging
 import subprocess
@@ -20,8 +20,8 @@ def command_exec_wrapper(cmd_list, prefix='', ssh_client=None,
     command and its arguments
     :param prefix: The prefix to be used for logging of executed command output
     :param ssh_client : SSH client provided by paramiko to run the command
-    :param data_queue: data queue where generator output is posted line by line
-    the generator process will run.
+    :param data_queue: data queue where cbench output is posted line by line
+    the cbench process will run.
     :returns: The commands exit status
     :rtype: int
     :type cmd_list: list<str>
@@ -39,45 +39,44 @@ def command_exec_wrapper(cmd_list, prefix='', ssh_client=None,
     return exit_status
 
 
-def rebuild_cbench(generator_build_handler, ssh_client=None):
-    """Rebuilds the generator.
+def rebuild_cbench(cbench_build_handler, ssh_client=None):
+    """Rebuilds cbench.
 
-    :param generator_build_handler: filepath to the handler that builds the
-    generator
+    :param cbench_build_handler: filepath to the handler that builds cbench
     :param ssh_client : SSH client provided by paramiko to run the command
-    :type generator_build_handler: str
+    :type cbench_build_handler: str
     :type ssh_client: paramiko.SSHClient
     """
 
-    command_exec_wrapper([generator_build_handler],
-                         '[generator_build_handler]', ssh_client)
+    command_exec_wrapper([cbench_build_handler],
+                         '[cbench_build_handler]', ssh_client)
 
 
 
-def run_cbench(generator_run_handler, controller_ip, controller_port, threads,
+def run_cbench(cbench_run_handler, controller_ip, controller_port, threads,
                sw_per_thread, switches, thr_delay_ms, traf_delay_ms,
                ms_per_test, internal_repeats, hosts, warmup, mode,
                data_queue=None, ssh_client=None):
-    """Runs a generator instance
+    """Runs a cbench instance
 
-    :param generator_run_handler: generator run handler
+    :param cbench_run_handler: cbench run handler
     :param controller_ip: controller IP for OpenFlow connection
     :param controller_port: controller port for OpenFlow connection
-    :param threads: number of generator threads
+    :param threads: number of cbench threads
     :param sw_per_thread: number of switches per thread
     :param switches: number of total switches
     :param thr_delay_ms: delay between thread creation (in milliseconds)
     :param traf_delay_ms: delay between last thread creation and traffic
     transmission (in milliseconds)
-    :param ms_per_test: test duration of a single generator loop
-    :param internal_repeats: number of generator loops
+    :param ms_per_test: test duration of a single cbench loop
+    :param internal_repeats: number of cbench loops
     :param hosts: number of simulated hoss
     :param warmup: initial loops to be considered as 'warmup'
-    :param mode: generator mode
-    :param data_queue: data queue where generator output is posted line by line
-    the generator process will run.
+    :param mode: cbench mode
+    :param data_queue: data queue where cbench output is posted line by line
+    the cbench process will run.
     :param ssh_client : SSH client provided by paramiko to run the command
-    :type generator_run_handler: str
+    :type cbench_run_handler: str
     :type controller_ip: str
     :type controller_port: int
     :type threads: int
@@ -94,57 +93,56 @@ def run_cbench(generator_run_handler, controller_ip, controller_port, threads,
     :type ssh_client: paramiko.SSHClient
     """
 
-    cmd_list = [generator_run_handler,
+    cmd_list = [cbench_run_handler,
                 controller_ip, str(controller_port), str(threads),
                 str(sw_per_thread), str(switches), str(thr_delay_ms),
                 str(traf_delay_ms), str(ms_per_test), str(internal_repeats),
                 str(hosts), str(warmup), mode]
-    command_exec_wrapper(cmd_list, '[generator_run_handler]', ssh_client,
+    command_exec_wrapper(cmd_list, '[cbench_run_handler]', ssh_client,
                          data_queue)
 
-def cleanup_cbench(generator_clean_handler, ssh_client=None):
+def cleanup_cbench(cbench_clean_handler, ssh_client=None):
     """Shuts down the Generator.
 
-    :param generator_clean_handler: Filepath to the handler that cleanup the
-    generator.
+    :param cbench_clean_handler: cleanup handler filepath for cbench.
     :param ssh_client : SSH client provided by paramiko to run the command
-    :type generator_clean_handler: str
+    :type cbench_clean_handler: str
     :type ssh_client: paramiko.SSHClient
     """
 
-    command_exec_wrapper([generator_clean_handler],
-                         '[generator_clean_handler]', ssh_client)
+    command_exec_wrapper([cbench_clean_handler],
+                         '[cbench_clean_handler]', ssh_client)
 
 
-def cbench_thread(generator_run_handler, controller_ip,
+def cbench_thread(cbench_run_handler, controller_ip,
                      controller_port, threads, sw_per_thread, switches,
                      thr_delay_ms, traf_delay_ms, ms_per_test, internal_repeats,
                      hosts, warmup, mode, cbench_node_ip, cbench_node_ssh_port,
                      cbench_node_username, cbench_node_password, succ_msg='',
                      fail_msg='', data_queue=None):
 
-    """ Function executed by generator thread.
+    """ Function executed by cbench thread.
 
-    :param generator_run_handler: generator run handler
+    :param cbench_run_handler: cbench run handler
     :param controller_ip: controller IP
     :param controller_port: controller port
-    :param threads: number of generator threads
+    :param threads: number of cbench threads
     :param sw_per_thread: number of switches per thread
     :param switches: number of total switches
     :param thr_delay_ms: delay between thread creation
     :param traf_delay_ms: delay between last thread creation and traffic
     transmission
-    :param ms_per_test: test duration of a single generator loop
-    :param internal_repeats: number of generator loops
+    :param ms_per_test: test duration of a single cbench loop
+    :param internal_repeats: number of cbench loops
     :param hosts: number of simulated hosts
     :param warmup: initial loops to be considered as 'warmup'
-    :param mode: generator mode
-    :param data_queue: data queue where generator output is posted line by line
-    :parar succ_msg: message written to data queue when generator_thread
+    :param mode: cbench mode
+    :param data_queue: data queue where cbench output is posted line by line
+    :parar succ_msg: message written to data queue when cbench_thread
     succeeds
-    :parar fail_msg: message written to data queue when generator_thread fails
+    :parar fail_msg: message written to data queue when cbench_thread fails
     :param ssh_client : SSH client provided by paramiko to run the command
-    :type generator_run_handler: str
+    :type cbench_run_handler: str
     :type controller_ip: str
     :type controller_port: int
     :type threads: int
@@ -163,29 +161,34 @@ def cbench_thread(generator_run_handler, controller_ip,
     :type ssh_client: paramiko.SSHClient
     """
 
-    logging.info('[generator_thread] Generator thread started')
+    logging.info('[cbench_thread] cbench thread started')
 
     try:
         # Opening connection with cbench_node_ip and returning
         # cbench_ssh_client to be utilized in the sequel
-        cbench_ssh_client = util.netutil.ssh_connect_or_return(cbench_node_ip.value.decode(),
-            cbench_node_username.value.decode(), cbench_node_password.value.decode(), 10,
+        cbench_ssh_client = \
+            util.netutil.ssh_connect_or_return(cbench_node_ip.value.decode(),
+                cbench_node_username.value.decode(),
+                cbench_node_password.value.decode(), 10,
             int(cbench_node_ssh_port.value.decode()))
 
-        run_cbench(generator_run_handler.value.decode(), controller_ip.value.decode(),
-            controller_port.value, threads.value, sw_per_thread.value, switches.value, thr_delay_ms.value,
-            traf_delay_ms.value, ms_per_test.value, internal_repeats.value, hosts.value, warmup.value, mode.value.decode(),
-            data_queue, cbench_ssh_client)
+        run_cbench(cbench_run_handler.value.decode(),
+                   controller_ip.value.decode(),
+                   controller_port.value, threads.value,
+                   sw_per_thread.value, switches.value, thr_delay_ms.value,
+                   traf_delay_ms.value, ms_per_test.value,
+                   internal_repeats.value, hosts.value, warmup.value,
+                   mode.value.decode(), data_queue, cbench_ssh_client)
 
-        # generator ended, enqueue termination message
+        # cbench ended, enqueue termination message
         if data_queue is not None:
             data_queue.put(succ_msg.value.decode(), block=True)
-        logging.info('[generator_thread] Generator thread ended successfully')
+        logging.info('[cbench_thread] cbench thread ended successfully')
     except subprocess.CalledProcessError as err:
         if data_queue is not None:
             data_queue.put(fail_msg.value.decode(), block=True)
-        logging.error('[generator_thread] Exception:{0}'.format(str(err)))
+        logging.error('[cbench_thread] Exception:{0}'.format(str(err)))
     except:
-        logging.error('[generator_thread] General exception in generator thread.')
+        logging.error('[cbench_thread] General exception: cbench thread.')
 
     return
