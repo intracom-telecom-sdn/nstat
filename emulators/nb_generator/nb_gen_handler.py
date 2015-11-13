292a2163e2ae7bdd421185790c636799aa18c508
Panagiotis Georgiopoulos <panageo@intracom-telecom.com>
2015-11-13 14:55:07 +0200

    fixing call of flow_master_thread in handler of nb_generator

--- emulators/nb_generator/nb_gen_handler.py
+++ emulators/nb_generator/nb_gen_handler.py
@@ -46,7 +46,7 @@ def northbound_generator():
     discovery_deadline_ms = sys.argv[9]
     auth_token = (sys.argv[10], sys.argv[11])
     print(nflows)
-    nb_generator_results = flow_master_thread(ctrl_ip, ctrl_port, nnodes,
+    nb_generator_results = nb_gen.flow_master_thread(ctrl_ip, ctrl_port, nnodes,
                                               nworkers, flow_template,
                                               op_delay_ms,
                                               delete_flag,
