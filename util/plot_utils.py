cbe22cd6aa04175f7b5ed2cc1ddcf30628baaee8
Panagiotis Georgiopoulos <panageo@intracom-telecom.com>
2015-11-30 15:12:41 +0200

    Fixing bug in plots after functional testing

--- util/plot_utils.py
+++ util/plot_utils.py
@@ -184,7 +184,7 @@ def plot_multi_errorbar(z_axis_key, plot_options):
         plots[z_value] = plot_errorbar_helper(plot_options, x_keys_sorted,
                                               z_value)
     # Add legend to plot, plt object
-    generate_legend(z_axis_key, plots)
+    generate_legend(z_axis_key, plots, plot_options.legend_position)
     finish_plotting(plot_options)
 
 
@@ -254,14 +254,14 @@ def plot_multi_scatter(y_values, z_axis_key, plot_options):
             marker_arg=next(plot_options.markers),
             color=next(plot_options.colors))
     # Add legend to plot, plt object
-    generate_legend(z_axis_key, plots)
+    generate_legend(z_axis_key, plots, plot_options.legend_position)
     finish_plotting(plot_options)
 
 
-def generate_legend(z_axis_key, plots):
+def generate_legend(z_axis_key, plots, position):
     plt.legend(list(plots.values()),
         [z_axis_key + ':' + str(k) for k in list(plots.keys())],
-        scatterpoints=1, loc=plot_options.legend_position, fontsize=8)
+        scatterpoints=1, loc=position, fontsize=8)
 
 def create_xy_dict_from_file(results_file, x_axis_key, y_axis_key):
     """Reads a json file and returns the contents of the file as a
