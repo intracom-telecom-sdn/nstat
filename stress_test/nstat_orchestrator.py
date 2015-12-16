afa21098a12e09e911ef293b772368b2d228d0b2
panageo <panageo@gmail.com>
2015-12-16 09:32:40 +0200

    enhance the plot producing procedure

--- stress_test/nstat_orchestrator.py
+++ stress_test/nstat_orchestrator.py
@@ -206,41 +206,47 @@ def main():
         logging.info('[nstat_orchestrator] Producing plots')
         num_plots = len(test_config['plots'])
         for plot_index in list(range(0, num_plots)):
-            plot_options = util.plot_utils.PlotOptions()
-            plot_options.xmin = test_config['plots'][plot_index]['x_min']
-            plot_options.xmax = test_config['plots'][plot_index]['x_max']
-            plot_options.ymin = test_config['plots'][plot_index]['y_min']
-            plot_options.ymax = test_config['plots'][plot_index]['y_max']
-            plot_options.x_axis_label = \
-                test_config['plots'][plot_index]['x_axis_label']
-            plot_options.y_axis_label = \
-                test_config['plots'][plot_index]['y_axis_label']
-            plot_options.out_fig = \
-                test_config['plots'][plot_index]['plot_filename'] + '.png'
-            plot_options.plot_title = \
-                test_config['plots'][plot_index]['plot_title']
-            plot_options.x_axis_fct = \
-                float(eval(test_config['plots'][plot_index]['x_axis_factor']))
-            plot_options.y_axis_fct = \
-                float(eval(test_config['plots'][plot_index]['y_axis_factor']))
-            if test_config['plots'][plot_index]['x_axis_scale'] == 'log':
-                plot_options.xscale_log = True
-            else:
-                plot_options.xscale_log = False
+            try:
+                plot_options = util.plot_utils.PlotOptions()
+                plot_options.xmin = test_config['plots'][plot_index]['x_min']
+                plot_options.xmax = test_config['plots'][plot_index]['x_max']
+                plot_options.ymin = test_config['plots'][plot_index]['y_min']
+                plot_options.ymax = test_config['plots'][plot_index]['y_max']
+                plot_options.x_axis_label = \
+                    test_config['plots'][plot_index]['x_axis_label']
+                plot_options.y_axis_label = \
+                    test_config['plots'][plot_index]['y_axis_label']
+                plot_options.out_fig = \
+                    test_config['plots'][plot_index]['plot_filename'] + '.png'
+                plot_options.plot_title = \
+                    test_config['plots'][plot_index]['plot_title']
+                plot_options.x_axis_fct = \
+                    float(eval(test_config['plots'][plot_index]['x_axis_factor']))
+                plot_options.y_axis_fct = \
+                    float(eval(test_config['plots'][plot_index]['y_axis_factor']))
+                if test_config['plots'][plot_index]['x_axis_scale'] == 'log':
+                    plot_options.xscale_log = True
+                else:
+                    plot_options.xscale_log = False
 
-            if test_config['plots'][plot_index]['y_axis_scale'] == 'log':
-                plot_options.yscale_log = True
-            else:
-                plot_options.yscale_log = False
-            # Call the util function responsible to generate the plot png
-            util.plot_json.plot_json(
-                args.json_output,
-                test_config['plots'][plot_index]['x_axis_key'],
-                test_config['plots'][plot_index]['y_axis_key'],
-                test_config['plots'][plot_index]['z_axis_key'],
-                test_config['plots'][plot_index]['plot_type'],
-                test_config['plots'][plot_index]['plot_subtitle_keys'],
-                plot_options)
+                if test_config['plots'][plot_index]['y_axis_scale'] == 'log':
+                    plot_options.yscale_log = True
+                else:
+                    plot_options.yscale_log = False
+                # Call the util function responsible to generate the plot png
+                util.plot_json.plot_json(
+                    args.json_output,
+                    test_config['plots'][plot_index]['x_axis_key'],
+                    test_config['plots'][plot_index]['y_axis_key'],
+                    test_config['plots'][plot_index]['z_axis_key'],
+                    test_config['plots'][plot_index]['plot_type'],
+                    test_config['plots'][plot_index]['plot_subtitle_keys'],
+                    plot_options)
+            except:
+                logging.error(
+                    '[nstat_orchestrator] The plot {0} could not be created. '
+                    'Please check configuration. Continuing to the next plot.'.
+                    format(test_config['plots'][plot_index]['plot_title']))
 
         logging.info(
             '[nstat_orchestrator] Creating output directory of test results.')
