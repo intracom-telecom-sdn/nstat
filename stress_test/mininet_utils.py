81a406c53f48c5f84cbdbf2cf5b00ff44c8f371b
Konstantinos Papadopoulos <konpap@intracom-telecom.com>
2016-01-04 15:52:32 +0200

    Docstrings corrections on mininet_utils

--- stress_test/mininet_utils.py
+++ stress_test/mininet_utils.py
@@ -42,7 +42,8 @@ def delete_mininet_handlers(mininet_node, mininet_folder):
     :param mininet_node: a named tuple with all the connection information.
     It must have the following elements: ['name', 'ip', 'ssh_port', 'username',
     'password']
-    :param mininet_folder:
+    :param mininet_folder: full of mininet folder (containing mininet handlers)
+    to be removed
     :type mininet_node: namedtuple<str,str,str,str,str>
     :type mininet_folder: str
     """
@@ -60,29 +61,27 @@ def init_mininet_topo(mininet_init_topo_handler, mininet_rest_server,
 
     :param mininet_init_topo_handler: full path of the handler to initialize
     the Mininet topology
-    :param mininet_rest_server_host: hostname/IP the REST server listens to
-    :param mininet_rest_server_port: port the REST server listens to
+    :param mininet_rest_server: named tuple containng 1) mininet_node_ip
+    2)mininet_rest_server_port the REST server listens to
     :param controller_ip: controller IP
     :param controller_port: controller OpenFlow port
     :param mininet_topology_type: Type of the network topology. It can have
     one of the following values (DisconnectedTopo,LinearTopo or MeshTopo)
-    :param mininet_size: Size of the Mininet tpology
-    :param mininet_group_size: Size of the Mininet Group
-    :param mininet_group_delay_ms: Delay in which Mininet groups are
+    :param mininet_size: size of the Mininet tpology
+    :param mininet_group_size: size of Mininet group (batches of switches)
+    :param mininet_group_delay_ms: delay in which Mininet groups are
     initialized
-    :param mininet_hosts_per_switch: number of Hosts each Mininet switch is
+    :param mininet_hosts_per_switch: number of hosts each Mininet switch is
     attached to
     :type mininet_init_topo_handler: str
-    :type mininet_rest_server_host: str
-    :type mininet_rest_server_port: int
+    :type mininet_rest_server: namedtuple<str,int>
     :type controller_ip: str
     :type controller_port: int
-    :type Topology: str
+    :type mininet_topology_type: str
     :type mininet_size: int
     :type mininet_group_size: int
-    :type mininet_group_delay: int
+    :type mininet_group_delay_ms: int
     :type mininet_hosts_per_switch: int
-    :type start_topo_command: list<str>
     """
 
     init_topo_command = [mininet_init_topo_handler, mininet_rest_server.ip,
@@ -106,33 +105,28 @@ def mininet_topo_check_booted(expected_switches, mininet_group_size,
     side and from the controller operational DS.
 
     :param expected_switches: expected Mininet switches
-    :param mininet_group_size: group size at which switches where added in a
+    :param mininet_group_size: group size at which switches are added in a
     Mininet topo
     :param mininet_group_delay_ms: delay between group additions
     (in milliseconds)
     :param mininet_get_switches_handler: Mininet handler used to query the
     current number of switches in a Mininet topology
-    :param mininet_ip: Mininet node IP
-    :param mininet_rest_server_port: port of the Mininet node REST server
-    :param ctrl_ip: controller IP
-    :param ctrl_port: controller RESTconf port
-    :param controller_restconf_user: RESTconf username
-    :param controller_restconf_password: RESTconf password
+    :param mininet_rest_server: named tuple containing 1) mininet_node_ip
+    2)mininet_rest_server_port the REST server listens to
+    :param controller_nb_interface: named tuple containing 1) controller_node_ip
+    2) controller_restconf_port 3) controller_restconf_user
+    4) controller_restconf_password
     :param num_tries: maximum number of tries until the method identifies that
     number of discovered switches of the Mininet topology is equal to the
     number of expected Mininet switches
-    :raises Exception: If we reach max number of tries and either the
+    :raises Exception: if we reach max number of tries and either the
     controller has not seen the topology or the topology has failed to start.
     :type expected_switches: int
     :type mininet_group_size: int
     :type mininet_group_delay_ms: int
     :type mininet_get_switches_handler: str
-    :type mininet_ip: str
-    :type mininet_rest_server_port: int
-    :type ctrl_ip: str
-    :type ctrl_port: int
-    :type controller_restconf_user: str
-    :type controller_restconf_password: str
+    :type mininet_rest_server: namedtuple<str,int>
+    :type controller_nb_interface: namedtuple<str,int,str,str>
     :type num_tries: int
     """
 
@@ -185,13 +179,13 @@ def start_mininet_server(mininet_ssh_session, mininet_server_remote_path,
     :param mininet_ssh_session: ssh session used to issue remote command
     :param mininet_server_remote_path: path where mininet_custom_boot.py is
     stored and used to start the Mininet topology.
-    :param mininet_rest_server_host: hostname/IP the REST server should listen
-    to
-    :param mininet_rest_server_port: port the REST server should listen to
+    :param mininet_rest_server: named tuple containing 1) mininet_node_ip
+    2)mininet_rest_server_port the REST server listens to
+    :raises Exception: if getpid_listeningonport() returns -1, Mininet rest
+    server failed to start
     :type mininet_ssh_session: ssh connection object
     :type mininet_server_remote_path: str
-    :type mininet_rest_server_host: str
-    :type mininet_rest_server_port: int
+    :type mininet_rest_server: namedtuple<str,int>
     """
 
     boot_command = ('sudo python {0} --rest-host {1} --rest-port {2}'.
@@ -216,7 +210,7 @@ def start_mininet_server(mininet_ssh_session, mininet_server_remote_path,
     if mininet_server_pid == -1:
         logging.error('{0} Fail to start mininet REST server.'.
                       format('[start_mininet_server]'))
-        raise Exception('Fail to start mininet REST server. Got invalid pid.')
+        raise Exception('Failed to start mininet REST server. Got invalid pid.')
 
 def start_stop_mininet_topo(mininet_start_stop_handler, mininet_rest_server,
                        mininet_action):
@@ -224,15 +218,14 @@ def start_stop_mininet_topo(mininet_start_stop_handler, mininet_rest_server,
     Locally calls the Mininet handler that remotely starts an initialized
     topology on a remote Mininet node
 
-    :param mininet_start_stop_handler: full path of the handler to start the Mininet
-    topology
-    :param mininet_rest_server_host: hostname/IP the REST server listens to
+    :param mininet_start_stop_handler: full path of the handler to start the
+    Mininet topology
     :param mininet_rest_server: named tuple containing the ip address of
     mininet REST server and the port it listens for requests
-    :param mininet_action: defines the operation that start-stop mininet
+    :param mininet_action: defines the operation that start/stop mininet
     handler will perform. Can have either 'start' or 'stop' value.
-    :type mininet_start_handler: str
-    :type mininet_rest_server: namedtuple
+    :type mininet_start_stop_handler: str
+    :type mininet_rest_server: namedtuple<str,int>
     :type mininet_action: str
     """
 
