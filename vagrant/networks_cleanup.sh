01418a2825339898b5ae0f8afcae3257b1650345
Konstantinos Papadopoulos <konpap@intracom-telecom.com>
2016-01-28 17:36:40 +0200

    Finalized networks_cleanup.sh

--- vagrant/networks_cleanup.sh
+++ vagrant/networks_cleanup.sh
@@ -10,14 +10,33 @@
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 echo $SCRIPT_DIR
 
+declare -a vmslist=("vm_mt_cbench" "vm_controller" "vm_mininet" "vm_nb_generator" "vm_nstat")
+
+for i in "${vmslist[@]}"; do
+	echo $i
+	vboxmanage controlvm $i poweroff
+	vboxmanage unregistervm $i --delete
+done
+
+
 for item in $( ls -1d $SCRIPT_DIR/*/ ); do
+	echo $item
 	cd $item
-	rm -rf .vagrant
 	vagrant destroy -f
+	virsh shutdown $item"_nstat_vm_packaged"
+    virsh destroy $item"_nstat_vm_packaged"
+    virsh undefine $item"_nstat_vm_packaged"
+    rm -rf .vagrant
 	cd $SCRIPT_DIR
 done
 
-vboxmanage hostonlyif remove 'vboxnet'
-virsh net-destroy --network 'vagrant-libvirt-nstat'
+for network in $(vboxmanage list hostonlyifs | egrep  '^Name:.*' | awk '{print $2}');
+do
+    vboxmanage hostonlyif remove $network
+done
 
-echo "[clean.sh] Cleanup of multinet completed successfully"
+virsh net-destroy --network vagrant-libvirt-nstat
+virsh net-undefine --network vagrant-libvirt-nstat
+
+
+echo "[networks_cleanup.sh] Cleanup of multinet completed successfully"
