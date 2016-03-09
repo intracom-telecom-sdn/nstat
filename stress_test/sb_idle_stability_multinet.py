668343eee50cc00a80ffc59f1d91e1c0212b8d3c
Konstantinos Papadopoulos <konpap@intracom-telecom.com>
2016-03-09 16:06:25 +0200

    Restored build of oftraf within stress tests

--- stress_test/sb_idle_stability_multinet.py
+++ stress_test/sb_idle_stability_multinet.py
@@ -170,11 +170,10 @@ def sb_idle_stability_multinet_run(out_json, ctrl_base_dir, multinet_base_dir,
         # OFTRAF actions:
         # 1. build OFTRAF (remember to uncomment when bugs are fixed in OFTRAF
         # repo)
-        """
         logging.info('{0} Building oftraf.'.format(test_type))
         oftraf_utils.oftraf_build(oftraf_handlers_set.oftraf_build_handler,
                                   controller_ssh_client)
-        """
+
 
         logging.info('{0} Changing controller statistics period to {1} ms'.
             format(test_type, controller_statistics_period_ms))
