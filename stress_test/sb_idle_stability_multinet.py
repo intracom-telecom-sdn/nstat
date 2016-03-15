964de429091e7f0bf68d5d4ff47c9925827a1a58
panageo <panageo@gmail.com>
2016-03-15 16:11:14 +0200

    comment out oftraf build and add changes to oftraf files inside the repo for stress testing

--- stress_test/sb_idle_stability_multinet.py
+++ stress_test/sb_idle_stability_multinet.py
@@ -170,9 +170,9 @@ def sb_idle_stability_multinet_run(out_json, ctrl_base_dir, multinet_base_dir,
         # OFTRAF actions:
         # 1. build OFTRAF (remember to uncomment when bugs are fixed in OFTRAF
         # repo)
-        logging.info('{0} Building oftraf.'.format(test_type))
-        oftraf_utils.oftraf_build(oftraf_handlers_set.oftraf_build_handler,
-                                  controller_ssh_client)
+        #logging.info('{0} Building oftraf.'.format(test_type))
+        #oftraf_utils.oftraf_build(oftraf_handlers_set.oftraf_build_handler,
+        #                          controller_ssh_client)
 
 
         logging.info('{0} Changing controller statistics period to {1} ms'.
