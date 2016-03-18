9514c4ddc9418914ad826be93745584e5a3f8f9f
panageo <panageo@gmail.com>
2016-03-18 13:11:25 +0200

    make commit to run functional test

--- stress_test/nb_active_scalability_multinet.py
+++ stress_test/nb_active_scalability_multinet.py
@@ -232,11 +232,11 @@ def nb_active_scalability_multinet_run(out_json, ctrl_base_dir,
             multinet_topo_size = \
                 multinet_worker_topo_size * len(multinet_worker_ip_list)
 
-            common.check_topo_booted(multinet_topo_size, multinet_group_size,
-                                     multinet_group_delay_ms,
-                                     multinet_handlers_set.get_switches_handler,
-                                     multinet_rest_server,
-                                     controller_nb_interface)
+            #common.check_topo_booted(multinet_topo_size, multinet_group_size,
+            #                         multinet_group_delay_ms,
+            #                         multinet_handlers_set.get_switches_handler,
+            #                         multinet_rest_server,
+            #                         controller_nb_interface)
 
             cmd = ('cd {0}; taskset -c {1} python3.4 {2} {3} {4} {5} {6} {7} {8} {9} {10} {11}'.
                 format(nb_generator_base_dir, nb_generator_cpus,
