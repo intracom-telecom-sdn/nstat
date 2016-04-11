bf3e755e3c63fc75f53aa6cc1d143b9667bdc883
Konstantinos Papadopoulos <konpap@intracom-telecom.com>
2016-04-11 14:07:48 +0300

    Finalized NSTAT mtcbench build handler

--- emulators/mt_cbench/build.sh
+++ emulators/mt_cbench/build.sh
@@ -15,10 +15,7 @@ MT_CBENCH_LOCATION="https://github.com/intracom-telecom-sdn/mtcbench.git"
 if [ ! -d $SCRIPT_DIR"/oflops" ] || [ ! -d $SCRIPT_DIR"/openflow" ]; then
    git clone $MT_CBENCH_LOCATION $SCRIPT_DIR"/mtcbench"
    cd $SCRIPT_DIR"/mtcbench"
-   git branch -a
-   git checkout develop_build_error
    mv $SCRIPT_DIR"/mtcbench/oflops" $SCRIPT_DIR | mv $SCRIPT_DIR"/mtcbench/openflow" $SCRIPT_DIR | mv $SCRIPT_DIR"/mtcbench/build_mtcbench.sh" $SCRIPT_DIR
-   cd $SCRIPT_DIR
    rm -rf $SCRIPT_DIR"/mtcbench"
 fi
 
