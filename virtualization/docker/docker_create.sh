5bb37286215690249aa54ec1ae16e87bb09561fd
Konstantinos Papadopoulos <konpap@intracom-telecom.com>
2016-06-30 16:35:46 +0300

    Finalized Dockerfile/Vagrantfile common provision.sh

--- virtualization/docker/docker_create.sh
+++ virtualization/docker/docker_create.sh
@@ -10,21 +10,6 @@
 # current directory (the "fast" path).
 # If it doesn't exist, it downloads it in /opt and them extracts it.
 
-CONTAINER_IMAGE='nstat'
-CONTAINER_NAME='container01'
-CONTAINER_SUBNET='192.168.100.0/24'
-CONTAINER_HOSTNAME='worker01'
-CONTAINER_NETWORK_NAME='docker-nstat'
-
-sudo docker network create --subnet=$CONTAINER_SUBNET -o --icc,--ip-masq,--mtu=1500 $CONTAINER_NETWORK_NAME
-sudo docker pull ubuntu:14.04
-sudo docker run -v $(pwd):/opt -dit --privileged --ulimit nofile=150000:150000 -h=$CONTAINER_HOSTNAME --name=$CONTAINER_NAME --net=$CONTAINER_NETWORK_NAME ubuntu:14.04 /bin/bash
-sudo docker build .
-
-sudo docker exec $CONTAINER_NAME  /bin/bash /opt/provision.sh
-
-sudo docker ps -a
-sudo docker commit $CONTAINER_NAME $CONTAINER_IMAGE
-
-# For creating further containers out of CONTAINER_NAME
-# sudo docker run -it --privileged -h=$CONTAINER_HOSTNAME --name=$CONTAINER_NAME --net=$CONTAINER_NETWORK_NAME $CONTAINER_IMAGE /bin/bash
+IMAGE_NAME='nstat'
+sudo docker build --no-cache -t $IMAGE_NAME .
+#sudo docker run -it nstat /bin/bash
