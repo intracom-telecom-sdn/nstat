4a12538772f70c2e4e82d4d08a1070d47c68a5b4
Thomas Sounapoglou <tomsou82@gmail.com>
2016-10-11 12:46:34 +0300

    Test monitor_threads_ds()

--- stress_test/class_test/nb_generator_test.py
+++ stress_test/class_test/nb_generator_test.py
@@ -155,7 +155,7 @@ for (multinet.topo_size,
     # run NB-generator
     first_REST_request = time.time()
     nb_generator.run()
-#    nb_generator.monitor_threads_run(first_REST_request)
+    nb_generator.monitor_threads_run(first_REST_request)
     print('End to End installation time is: {0}'.format(nb_generator.e2e_installation_time))
     print('Confirmation time is: {0}'.format(nb_generator.confirm_time))
     print('Time to discover flows on switches is: {0}'.format(nb_generator.discover_flows_on_switches_time))
