caf6631770af9256e80df7f08df08281f4385411
panageo <panageo@gmail.com>
2016-10-19 11:27:25 +0300

    Add packer deployment capability

---
+++ deploy/packer/installation.md
@@ -0,0 +1,12 @@
+## Creating a base image with packer
+
+In order to create a base image using `packer` you must
+
+1.  Install packer following the [install packer guide](https://www.packer.io/docs/installation.html)
+2.  Edit the file `deploy/packer/scripts/update.sh` and configure the
+    `http_proxy` variable according to the proxy settings of your environment.
+    Leave it empty in case you do not have a proxy.
+3.  From the path `deploy/packer` run the following command:
+    ```bash
+    packer build ubuntu-14.04-server-amd64.json
+    ```
---
+++ deploy/packer/scripts/update.sh
@@ -0,0 +1,26 @@
+# Update the box
+useradd -m -s /bin/bash -p $(openssl passwd -crypt adminubuntu) -U adminubuntu
+echo "adminubuntu ALL=(ALL) NOPASSWD:ALL" | tee -a /etc/sudoers
+
+http_proxy=http://172.28.40.9:3128/
+https_proxy=$http_proxy
+
+# Bipass proxy settings if http_proxy is empty
+if [ ! -z "$http_proxy" ]
+then
+    export http_proxy
+    export https_proxy
+
+    echo "http_proxy=$http_proxy" | sudo tee -a /etc/environment
+    echo "https_proxy=$https_proxy" | sudo tee -a /etc/environment
+    echo "HTTP_PROXY=$http_proxy" | sudo tee -a /etc/environment
+    echo "HTTPS_PROXY=$https_proxy" | sudo tee -a /etc/environment
+    if [ ! -f /etc/apt/apt.conf ]; then
+        sudo touch /etc/apt/apt.conf
+    fi
+    echo 'Acquire::http::Proxy "'$http_proxy'";' | sudo tee -a /etc/apt/apt.conf
+    echo 'Acquire::https::Proxy "'$https_proxy'";' | sudo tee -a /etc/apt/apt.conf
+fi
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
