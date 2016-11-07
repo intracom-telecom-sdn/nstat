eb8a940a5874eb97f7da6b8ccee656478d6f1d13
Thomas Sounapoglou <tomsou82@gmail.com>
2016-11-07 16:51:07 +0200

    Add sb_active_scalability_cbench case

--- stress_test/report_spec_get.py
+++ stress_test/report_spec_get.py
@@ -7,6 +7,107 @@
 import report_spec
 
 
+def sb_active_scalability_cbench(test_type, config_json, results_json):
+    """It returns all the information that is needed for the generation of the
+    report for the specific test.
+
+    :param test_type: describes the type of the specific test. This value
+    defines the title of the html report.
+    :param config_json: This is the filepath to the configuration json file.
+    :param results_json: This is the filepath to the results json file.
+    :returns: A ReportSpec object that holds all the test report information
+    and is passed as input to the generate_html() function in the
+    html_generation.py, that is responsible for the report generation.
+    :rtype: ReportSpec object
+    :type: test_type: str
+    :type: config_json: str
+    :type: results_json: str
+    """
+
+    report_spec_obj = report_spec.ReportSpec(
+        config_json, results_json, '{0}'.format(test_type),
+        [report_spec.TableSpec(
+            '1d', 'Test configuration parameters (detailed)',
+            [('test_repeats', 'Test repeats'),
+             ('controller_name', 'Controller name'),
+             ('controller_build_handler', 'Controller build script'),
+             ('controller_start_handler', 'Controller start script'),
+             ('controller_stop_handler', 'Controller stop script'),
+             ('controller_status_handler', 'Controller status script'),
+             ('controller_clean_handler', 'Controller cleanup script'),
+             ('controller_statistics_handler', 'Controller statistics script'),
+             ('controller_node_ip', 'Controller IP node address'),
+             ('controller_node_ssh_port', 'Controller node ssh port'),
+             ('controller_node_username', 'Controller node username'),
+             ('controller_node_password', 'Controller node password'),
+             ('controller_port', 'Controller Southbound port'),
+             ('controller_rebuild', 'Controller rebuild between test repeats'),
+             ('controller_logs_dir', 'Controller log save directory'),
+             ('cbench_name', 'Generator name'),
+             ('cbench_node_ip', 'Cbench node IP address'),
+             ('cbench_node_ssh_port', 'Cbench node ssh port'),
+             ('cbench_node_username', 'Cbench node username'),
+             ('cbench_node_password', 'Cbench node password'),
+             ('cbench_build_handler', 'Generator build script'),
+             ('cbench_run_handler', 'Generator start script'),
+             ('cbench_clean_handler', 'Generator cleanup script'),
+             ('cbench_simulated_hosts', 'Generator simulated hosts'),
+             ('cbench_threads', 'Generator threads'),
+             ('cbench_thread_creation_delay_ms',
+              'Generation delay in ms between thread creation'),
+             ('cbench_switches_per_thread',
+              'Switches per generator thread'),
+             ('cbench_internal_repeats', 'Generator internal repeats'),
+             ('cbench_ms_per_test', 'Internal repeats duration in ms'),
+             ('cbench_rebuild',
+              'Generator rebuild between each test repeat'),
+             ('cbench_mode', 'Generator testing mode'),
+             ('cbench_warmup', 'Generator warmup repeats'),
+             ('cbench_delay_before_traffic_ms',
+              'Generator delay before sending traffic in ms'),
+             ('java_opts', 'JVM options')
+            ], config_json)],
+        [report_spec.TableSpec('2d', 'Test results',
+            [('global_sample_id', 'Sample ID'),
+             ('timestamp', 'Sample timestamp (seconds)'),
+             ('date', 'Sample timestamp (date)'),
+             ('test_repeats', 'Total test repeats'),
+             ('repeat_id', 'External repeat ID'),
+             ('cbench_internal_repeats', 'Generator Internal repeats'),
+             ('internal_repeat_id', 'Internal repeat ID'),
+             ('throughput_responses_sec', 'Throughput (responses/sec)'),
+             ('cbench_simulated_hosts', 'Generator simulated hosts'),
+             ('cbench_switches', 'Generated simulated switches'),
+             ('cbench_threads', 'Generator threads'),
+             ('cbench_switches_per_thread',
+              'Switches per generator thread'),
+             ('cbench_thread_creation_delay_ms',
+              'Delay between thread creation (ms)'),
+             ('cbench_delay_before_traffic_ms',
+              'Delay before PacketIn transmission (ms)'),
+             ('cbench_ms_per_test', 'Internal repeats interval'),
+             ('cbench_warmup', 'Cbench warmup repeats'),
+             ('cbench_mode', 'Cbench test mode'),
+             ('mtcbench_cpu_shares', 'MT-Cbench CPU percentage'),
+             ('controller_node_ip', 'Controller IP node address'),
+             ('controller_port', 'Controller port'),
+             ('controller_java_xopts', 'Java options'),
+             ('one_minute_load', 'One minute load'),
+             ('five_minute_load', 'five minutes load'),
+             ('fifteen_minute_load', 'fifteen minutes load'),
+             ('used_memory_bytes', 'System used memory (Bytes)'),
+             ('total_memory_bytes', 'Total system memory'),
+             ('controller_cpu_shares', 'Controller CPU percentage'),
+             ('controller_cpu_system_time', 'Controller CPU system time'),
+             ('controller_cpu_user_time', 'Controller CPU user time'),
+             ('controller_num_threads', 'Controller threads'),
+             ('controller_num_fds', 'Controller num of fds'),
+             ('controller_statistics_period_ms',
+              'Controller statistics period (ms)')
+            ], results_json)])
+    return report_spec_obj
+
+
 def sb_active_stability_cbench(test_type, config_json, results_json):
     """It returns all the information that is needed for the generation of the
     report for the specific test.
