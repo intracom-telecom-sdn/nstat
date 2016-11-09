7b51394639497789184ae30979ac78e8dacf8b50
Konstantinos Papadopoulos <konpap@intracom-telecom.com>
2016-11-09 13:43:59 +0200

    Add missing import of test_type class

--- stress_test/nstat_orchestrator_new.py
+++ stress_test/nstat_orchestrator_new.py
@@ -14,6 +14,7 @@ import argparse
 import json
 import stress_test.controller
 import stress_test.emulator
+import stress_test.test_type
 
 def main():
     """Main function where NSTAT test application starts.
