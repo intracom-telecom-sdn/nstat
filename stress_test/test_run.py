b48a08ed12e704028d64401858bb3072d1bc5b83
thomas sounapoglou <soth@intracom-telecom.com>
2016-11-11 11:11:40 +0200

    Resolve missing import

--- stress_test/test_run.py
+++ stress_test/test_run.py
@@ -11,6 +11,7 @@ import stress_test.controller
 import stress_test.emulator
 import stress_test.monitor
 import stress_test.oftraf
+import stress_test.test_type
 import sys
 import time
 
@@ -28,13 +29,14 @@ class TestRun:
                                                 json_conf)
         self.test = stress_test.test_type.TestType(self, args)
 
-'''
+        '''
         if self.sb_emu. == MTCBENCH
             self.mon = stress_test.monitor.Mtcbench(self, self.ctrl, self.sb_emu)
         elif: self.sb_emu. == MULTINET
             self.mon = stress_test.monitor.Multinet(self, self.ctrl, self.sb_emu)
         else
-'''
+        '''
+
     def sb_active_scalability_cbench_run(self, json_conf,
                                          json_output,
                                          output_dir):
