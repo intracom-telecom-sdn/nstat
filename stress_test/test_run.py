c991a962ce76e44b53d03e6fd608abb542d7a428
Konstantinos Papadopoulos <konpap@intracom-telecom.com>
2016-11-14 13:12:04 +0200

    Correct key names within for loop

--- stress_test/test_run.py
+++ stress_test/test_run.py
@@ -50,14 +50,7 @@ class TestRun:
         #-------------------------------------------------------------------
         self.sb_emu.init_ssh()
         self.sb_emu.build()
-        print("Kostas Papadopoulos (before)",json_conf['cbench_threads'])
-        print(json_conf['cbench_threads'])
-        print(json_conf['cbench_switches_per_thread'])
-        print(json_conf['cbench_thread_creation_delay_ms'])
-        print(json_conf['cbench_delay_before_traffic_ms'])
-        print(json_conf['cbench_simulated_hosts'])
-        print(json_conf['test_repeats'])
-        print(json_conf['controller_statistics_period_ms'])
+
         # TEST run
         #-------------------------------------------------------------------
         for (self.sb_emu.threads,
@@ -66,11 +59,11 @@ class TestRun:
              self.sb_emu.delay_before_traffic_ms,
              self.sb_emu.simulated_hosts,
              self.repeat_id,
-             self.ctrl.stat_period) in itertools.product(json_conf['cbench_threads'],
-                               json_conf['cbench_switches_per_thread'],
-                               json_conf['cbench_thread_creation_delay_ms'],
-                               json_conf['cbench_delay_before_traffic_ms'],
-                               json_conf['cbench_simulated_hosts'],
+             self.ctrl.stat_period) in itertools.product(json_conf['mtcbench_threads'],
+                               json_conf['mtcbench_switches_per_thread'],
+                               json_conf['mtcbench_thread_creation_delay_ms'],
+                               json_conf['mtcbench_delay_before_traffic_ms'],
+                               json_conf['mtcbench_simulated_hosts'],
                                list(range(0, json_conf['test_repeats'])),
                                json_conf['controller_statistics_period_ms']):
             print("Kostas Papadopoulos (within loop)")
