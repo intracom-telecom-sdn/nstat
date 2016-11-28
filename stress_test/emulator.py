5e6b0df1de14a983e1f958ecb2e0ddf40b1416b9
Konstantinos Papadopoulos <konpap@intracom-telecom.com>
2016-11-28 14:30:07 +0200

    Removed redundant import

--- stress_test/emulator.py
+++ stress_test/emulator.py
@@ -3,7 +3,6 @@
 # This program and the accompanying materials are made available under the
 # terms of the Eclipse Public License v1.0 which accompanies this distribution,
 # and is available at http://www.eclipse.org/legal/epl-v10.html
-from docker_registry.core.exceptions import FileNotFoundError
 
 """ SB-Emulator Class- All SB-Emulator-related functionality is here"""
 
