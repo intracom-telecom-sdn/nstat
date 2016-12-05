67a95447e2867265b0d1d0f64bddbefd9fdd44f2
panageo <panageo@gmail.com>
2016-12-05 14:57:50 +0200

    update test dimension

--- stress_test/test_run.py
+++ stress_test/test_run.py
@@ -88,14 +88,11 @@ class TestRun:
              self.ctrl.stat_period_ms
              ) in itertools.product(json_conf['mtcbench_threads'],
                                     json_conf['mtcbench_switches_per_thread'],
-                                    json_conf['mtcbench_thread_creation_'
-                                              'delay_ms'],
-                                    json_conf['mtcbench_delay_before_traffic'
-                                              '_ms'],
+                                    json_conf['mtcbench_thread_creation_delay_ms'],
+                                    json_conf['mtcbench_delay_before_traffic_ms'],
                                     json_conf['mtcbench_simulated_hosts'],
                                     list(range(0, json_conf['test_repeats'])),
-                                    json_conf['controller_statistics_period_'
-                                              'ms']):
+                                    json_conf['controller_statistics_period_ms']):
             self.mon.global_sample_id = global_sample_id
             self.mon.repeat_id = repeat_id
             self.mon.test_repeats = json_conf['test_repeats']
