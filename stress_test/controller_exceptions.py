bc5b5a61baa6c83fc967ef08da54ca3831323be3
thomas sounapoglou <soth@intracom-telecom.com>
2017-01-11 15:46:08 +0200

    Update docstrings in controller_exceptions

--- stress_test/controller_exceptions.py
+++ stress_test/controller_exceptions.py
@@ -8,8 +8,14 @@
 
 
 class CtrlError(Exception):
-    """Base-class for all controller exceptions raised by this module."""
+
     def __init__(self, err_msg=None, err_code=1):
+        """Base-class for all controller exceptions raised by this module.
+        :param err_msg: the general error message.
+        :param err_code: the specific error code.
+        :type str
+        :type int
+        """
         self.err_code = err_code
         if err_msg is None:
             Exception.__init__(self, 'Controller generic exception')
@@ -20,120 +26,211 @@ class CtrlError(Exception):
 
 
 class CtrlNodeConnectionError(CtrlError):
-    """A controller node connection error."""
+
     def __init__(self, additional_error_info='', err_code=1):
+        """A controller node connection error.
+        :param additional_error_info: the general error message.
+        :param err_code: the specific error code.
+        :type str
+        :type int
+        """
         CtrlError.__init__(self, 'Fail to establish ssh connection with '
                                  'controller node. {0}'.
                                  format(additional_error_info), err_code)
 
 
 class CtrlBuildError(CtrlError):
-    """A controller build error."""
+
     def __init__(self, additional_error_info='', err_code=1):
+        """A controller build error
+        :param additional_error_info: the general error message.
+        :param err_code: the specific error code.
+        :type str
+        :type int
+        """
         CtrlError.__init__(self, 'Controller build failure. {0}'.
                                  format(additional_error_info), err_code)
 
 
 class CtrlStartError(CtrlError):
-    """A controller start error."""
+
     def __init__(self, additional_error_info='', err_code=1):
+        """A controller start error.
+        :param additional_error_info: the general error message.
+        :param err_code: the specific error code.
+        :type str
+        :type int
+        """
         CtrlError.__init__(self, 'Controller start failure. {0}'.
                                  format(additional_error_info), err_code)
 
 
 class CtrlStopError(CtrlError):
-    """A controller stop error."""
+
     def __init__(self, additional_error_info='', err_code=1):
+        """A controller stop error.
+        :param additional_error_info: the general error message.
+        :param err_code: the specific error code.
+        :type str
+        :type int
+        """
         CtrlError.__init__(self, 'Controller stop failure. {0}'.
                                  format(additional_error_info), err_code)
 
 
 class CtrlCleanupError(CtrlError):
-    """A controller cleanup error."""
+
     def __init__(self, additional_error_info='', err_code=1):
+        """A controller cleanup error.
+        :param additional_error_info: the general error message.
+        :param err_code: the specific error code.
+        :type str
+        :type int
+        """
         CtrlError.__init__(self, 'Controller cleanup failure. {0}'.
                                  format(additional_error_info), err_code)
 
 
 class CtrlStatusUnknownError(CtrlError):
-    """A controller status unknown error."""
+
     def __init__(self, additional_error_info='', err_code=1):
+        """A controller status unknown error.
+        :param additional_error_info: the general error message.
+        :param err_code: the specific error code.
+        :type str
+        :type int
+        """
         CtrlError.__init__(self, 'Controller fail to get status. {0}'.
                                  format(additional_error_info), err_code)
 
 
 class CtrlReadyStateError(CtrlError):
-    """A controller ready state error."""
+
     def __init__(self, additional_error_info='', err_code=1):
+        """A controller ready state error."""
         CtrlError.__init__(self, 'Controller did not reach '
                                  'ready state. {0}'.
                                  format(additional_error_info), err_code)
 
 
 class CtrlPortConflictError(CtrlError):
-    """A controller port conflict error."""
+
     def __init__(self, additional_error_info='', err_code=1):
+        """A controller port conflict error.
+        :param additional_error_info: the general error message.
+        :param err_code: the specific error code.
+        :type str
+        :type int
+        """
         CtrlError.__init__(self, 'Controller SouthBound port is in '
                                  'use by another process. {0}'.
                                  format(additional_error_info), err_code)
 
 
 class ODLXMLError(CtrlError):
-    """ODL XML generation error."""
+
     def __init__(self, additional_error_info='', err_code=1):
+        """ODL XML generation error.
+        :param additional_error_info: the general error message.
+        :param err_code: the specific error code.
+        :type str
+        :type int
+        """
         CtrlError.__init__(self, 'ODL Fail to generate XML files. {0}'.
                                  format(additional_error_info), err_code)
 
 
 class ODLDisablePersistenceError(CtrlError):
-    """ODL fail to disable persistence error."""
+
     def __init__(self, additional_error_info='', err_code=1):
+        """ODL fail to disable persistence error.
+        :param additional_error_info: the general error message.
+        :param err_code: the specific error code.
+        :type str
+        :type int
+        """
         CtrlError.__init__(self, 'ODL Fail to disable persistence mode. {0}'.
                                  format(additional_error_info), err_code)
 
 
 class ODLChangeStats(CtrlError):
-    """ODL fail to change statistics period error."""
+
     def __init__(self, additional_error_info='', err_code=1):
+        """ODL fail to change statistics period error.
+        :param additional_error_info: the general error message.
+        :param err_code: the specific error code.
+        :type str
+        :type int
+        """
         CtrlError.__init__(self, 'ODL Fail to change statistics period. {0}'.
                                  format(additional_error_info), err_code)
 
 
 class ODLFlowModConfError(CtrlError):
-    """ODL fail to configure for flow modifications send error."""
+
     def __init__(self, additional_error_info='', err_code=1):
+        """ODL fail to configure for flow modifications send error.
+        :param additional_error_info: the general error message.
+        :param err_code: the specific error code.
+        :type str
+        :type int
+        """
         CtrlError.__init__(self, 'ODL Fail to make configuration to send '
                            'flow modifications. {0}'.
                            format(additional_error_info), err_code)
 
 
 class ODLGetOperHostsError(CtrlError):
-    """ODL fail to get hosts from operational datastore error."""
+
     def __init__(self, additional_error_info='', err_code=1):
+        """ODL fail to get hosts from operational datastore error.
+        :param additional_error_info: the general error message.
+        :param err_code: the specific error code.
+        :type str
+        :type int
+        """
         CtrlError.__init__(self, 'ODL fail to get hosts from '
                            'operational datastore. {0}'.
                            format(additional_error_info), err_code)
 
 
 class ODLGetOperFlowsError(CtrlError):
-    """ODL fail to get installed flows from operational datastore error."""
+
     def __init__(self, additional_error_info='', err_code=1):
+        """ODL fail to get installed flows from operational datastore error.
+        :param additional_error_info: the general error message.
+        :param err_code: the specific error code.
+        :type str
+        :type int
+        """
         CtrlError.__init__(self, 'ODL fail to get installed flows from '
                            'operational datastore. {0}'.
                            format(additional_error_info), err_code)
 
 
 class ODLGetOperSwitchesError(CtrlError):
-    """ODL fail to get topology switches from operational datastore error."""
+
     def __init__(self, additional_error_info='', err_code=1):
+        """ODL fail to get topology switches from operational datastore error.
+        :param additional_error_info: the general error message.
+        :param err_code: the specific error code.
+        :type str
+        :type int
+        """
         CtrlError.__init__(self, 'ODL fail to get topology switches from '
                            'operational datastore. {0}'.
                            format(additional_error_info), err_code)
 
 
 class ODLGetOperLinksError(CtrlError):
-    """ODL fail to get topology links from operational datastore error."""
+
     def __init__(self, additional_error_info='', err_code=1):
+        """ODL fail to get topology links from operational datastore error.
+        :param additional_error_info: the general error message.
+        :param err_code: the specific error code.
+        :type str
+        :type int
+        """
         CtrlError.__init__(self, 'ODL fail to get topology links from '
                            'operational datastore. {0}'.
                            format(additional_error_info), err_code)
