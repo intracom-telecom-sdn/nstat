acf4e33f5b914077e7084dca34527eb37079bbe2
panageo <panageo@gmail.com>
2017-02-01 14:08:21 +0200

    fixes in sysstats unittests

--- util/unittests/test_sysstats.py
+++ util/unittests/test_sysstats.py
@@ -163,7 +163,7 @@ class ProcVariousPidRelatedTests(unittest.TestCase):
             stdout=cls.useless_file_htop_local,
             stderr=cls.useless_file_htop_local)
         cls.htop_pid_local = cls.htop_process_local.pid
-        cls.cur_dir_local = os.path.dirname(os.path.realpath(__file__))
+        cls.cur_dir_local = subprocess.check_output(['pwd'])
         cls.total_cpus_local = int(os.popen('nproc').read())
         cls.cmd_local = util.sysstats.proc_cmdline(cls.htop_pid_local)
         util.netutil.ssh_run_command(
