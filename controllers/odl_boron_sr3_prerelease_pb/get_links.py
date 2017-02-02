939d928e7de0e79e6f84f33f55eeeecff21f4016
panageo <panageo@gmail.com>
2017-02-02 13:19:18 +0200

    add sr3 prerelease

---
+++ controllers/odl_boron_sr3_prerelease_pb/get_links.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python3.4
+
+"""
+This handler returns the number of links of a topology, connected
+to the controller. This information is extracted from controller's operational
+datastore, using RESTCONF.
+"""
+
+import sys
+import logging
+import requests
+
+
+def get_oper_links():
+    """
+    Query number of links registered in ODL operational DS
+
+    :returns: number of links found, 0 if none exists and -1 in case of error.
+    :rtype: int
+    """
+
+    ip = sys.argv[1]
+    port = sys.argv[2]
+    username = sys.argv[3]
+    password = sys.argv[4]
+
+    url = ('http://{0}:{1}/restconf/operational/network-topology:'
+           'network-topology/network-topology:topology/flow:1/'.
+           format(ip, port))
+    s = requests.Session()
+    s.trust_env = False
+    auth_token = (username, password)
+    try:
+        datastore = s.get(url=url,
+                          auth=auth_token).json()['topology'][0]
+    except:
+        logging.error('[get_oper_links] Fail response from operational DS')
+        return -1
+    links = [link for link in datastore.get('link', [])]
+    logging.debug('[get_oper_links] Discovered links: {0}'.format(len(links)))
+    print(len(links))
+
+if __name__ == '__main__':
+    get_oper_links()
---
+++ controllers/odl_boron_sr3_prerelease_pb/xml_utils.py
@@ -0,0 +1,40 @@
+"""
+This handler changes a xml parameter file and creates a new one according to \
+    input string which is going to be updated and a new value string
+"""
+
+from lxml import etree
+import os
+import sys
+
+
+def manipulate_xml(input_filename, output_filename,
+                   string_to_find, target_value):
+    """
+    It gets a configuration (.xml in our case) file, defined by the \
+        input_filename parameter and changes its contents defied by the \
+        string_to_find parameter. The new values are defined in the \
+        target_value parameter. The result is saved in a new file defined by \
+        the output_file.
+
+    :param input_filename: The configuration filepath we want to change.
+    :param output_filename: The new configuration file, after changing the \
+        original one.
+    :param string_to_find: The string we want to change in the configuration \
+        file.
+    :param target_value: The new value to replace the string_to_find
+    :type input_filename: str
+    :type output_filename: str
+    :type string_to_find: str
+    :type target_value: str
+    """
+
+    if os.path.isfile(input_filename):
+        doc = etree.parse(input_filename)
+        for elt in doc.getiterator():
+            if str(elt.tag).find(string_to_find) != -1:
+                elt.text = target_value
+        outFile = open(output_filename, 'wb')
+        doc.write(outFile)
+    else:
+        sys.exit(1)
